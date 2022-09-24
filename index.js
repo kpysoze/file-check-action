2a321147e48dac98a6a3443424603c9bb921b2d0
kpysoze <kpysose@gmail.com>
2022-09-23 17:00:29 -0700

    Add line terminator

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const github = require('@actions/github');
 {
     async () => {
         try {
-            core.notice("Calling Custom Action")
+            core.notice("Calling Custom Action");
             core.setOutput("Hello World from Custom Action");
         } catch (error){
             core.setFailed("Custom Action Failed");
