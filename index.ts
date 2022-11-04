30284cadc320ee89a58c5d7d0ea0832307e001e6
kpysoze <65412973+kpysoze@users.noreply.github.com>
2022-11-03 18:13:51 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -7,13 +7,20 @@ const github = require('@actions/github');
             core.notice("Calling Custom Action");
             core.info("Hello World from Custom Action");
             
-            const myMap = new Map([
-                ["key1", "value1"],
-                ["key2", "value2"]
-            ]);
-            core.info(myMap.has("key1"))
-            core.info(myMap.has("key3"))
-
+            //const myMap = new Map([
+            //    ["key1", "value1"],
+            //    ["key2", "value2"]
+            //]);
+            //core.info(myMap.has("key1"))
+            //core.info(myMap.has("key3"))
+			
+			let dictionary = new Map<string, string>();
+			dictionary.set("one", "pending");
+			dictionary.set("two", "pending");
+			dictionary.set("three", "pending");
+			
+			core.info(dictionary.has("one"))
+            core.info(dictionary.has("four"))
             
         } catch (error){
             core.setFailed("Custom Action Failed");
