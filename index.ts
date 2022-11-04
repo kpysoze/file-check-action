3609e29d4476b335f68204380273673887bc7278
kpysoze <65412973+kpysoze@users.noreply.github.com>
2022-11-03 18:42:03 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -6,10 +6,9 @@ const github = require('@actions/github');
             core.notice("Calling Custom Action");
             core.info("Hello World from Custom Action");
             
-            const myMap = new Map([
-                ["key1", "value1"],
-                ["key2", "value2"]
-            ]);
+            const myMap = new Map();
+             myMap.set("key1", "1")
+             myMap.set("key2", "2")
             core.info(myMap.has("key1"))
             core.info(myMap.has("key3"))
 
