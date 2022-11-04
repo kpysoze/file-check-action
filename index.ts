b323fe769e0972b150087285ecfbdc5cf1703721
kpysoze <65412973+kpysoze@users.noreply.github.com>
2022-11-03 18:10:50 -0700

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,22 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+(
+    async () => {
+        try {
+            core.notice("Calling Custom Action");
+            core.info("Hello World from Custom Action");
+            
+            const myMap = new Map([
+                ["key1", "value1"],
+                ["key2", "value2"]
+            ]);
+            core.info(myMap.has("key1"))
+            core.info(myMap.has("key3"))
+
+            
+        } catch (error){
+            core.setFailed("Custom Action Failed");
+        }
+    }
+)();
